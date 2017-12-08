1fb7e4493425b224d26979429dfc9077343f99f7
Kynan <k.song@blueyonder.co.uk>
2017-12-08 10:53:50 +0000

    Passed first 3 tests.

--- specs/hero_specs.js
+++ specs/hero_specs.js
@@ -6,11 +6,19 @@ describe('Hero', function() {
   let hero;
 
   beforeEach(function() {
-    hero = new Hero("Smaug", "Cheese");
+    hero = new Hero("Burnt Face Man", "Cheese");
   })
 
   it("Should have a name", function() {
-    assert.strictEqual(hero.name, "Smaug")
+    assert.strictEqual(hero.name, "Burnt Face Man")
+  });
+
+  it("Should have health", function() {
+    assert.strictEqual(hero.health, 100)
+  });
+
+  it("Should have favourite food", function() {
+    assert.strictEqual(hero.favouriteFood, "Cheese")
   });
 
 
